3483629013ef5c53d95ef30737023a985098f799
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-21 08:13:27 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://thomasmehari2721:mvDrBXUsCvA1nWhm@books-store-mern.oikhe.mongodb.net/?retryWrites=true&w=majority&appName=Books-store-MERN"
+      "mongodb+srv://thomasmehari2721:XAFDaupAiA7hrTzT@books-store-mern.oikhe.mongodb.net/?retryWrites=true&w=majority&appName=Books-store-MERN"
     );
     console.log("Database Connected");
   } catch (error) {
