70aa5c848d4ffffd786c395ce513cee27c9d6ccc
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-21 08:44:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ origin: 'https://th-front-vx2e.vercel.app' }));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
